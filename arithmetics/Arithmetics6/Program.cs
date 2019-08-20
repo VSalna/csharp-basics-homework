a4a8dbc05ccea31d33cbd24053f13ead69be171b
Viesturs Salna <viesturssalna@gmail.com>
2019-08-20 10:48:45 +0300

    Added arithmetics excercises

---
+++ arithmetics/Arithmetics6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetics6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CozaLozaWoza();
+            Console.ReadKey();
+                
+
+        }
+
+        static void CozaLozaWoza()
+        {
+
+            for (int i = 1; i <= 110; i++)
+            {
+                
+                if (i % 11 == 0)
+                    Console.Write("\n");
+
+                if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+                    
+                {
+                    Console.Write(" CozaLozaWoza ");
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write(" CozaLoza ");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write(" Loza ");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write(" Coza ");
+
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write(" Woza ");
+
+                }
+                else
+                {
+                    Console.Write(" " + i);
+                }
+            }
+
+        }
+    }
+}
---
+++ arithmetics/Arithmetics9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetics9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(BodyMassIndex(176.3 ,70.8));
+            Console.ReadKey();
+        }
+
+        static double BodyMassIndex(double weight, double height)
+        {
+            double bmi = weight * 703 / Math.Pow(height, 2);
+
+            if (bmi < 18)
+            {
+                Console.WriteLine("Underweight");
+            }
+            if (bmi >= 18.5 && bmi <= 25)
+            {
+                Console.WriteLine("Optimal weight");
+            }
+            if (bmi > 25)
+            {
+                Console.WriteLine("Overweight");
+            }        
+            
+             
+            
+            return bmi;
+            
+        }
+    }
+}
