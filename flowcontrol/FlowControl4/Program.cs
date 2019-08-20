f1dbd760f9d990fa49b74644503548268ff6bf27
Viesturs Salna <viesturssalna@gmail.com>
2019-08-20 11:24:06 +0300

    added excercises

---
+++ flowcontrol/FlowControl4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintDayInWord();
+            Console.ReadKey();
+
+        }
+
+        static void PrintDayInWord()
+        {
+
+            int dayNumber = 1;
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ flowcontrol/TenBillion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TenBillion
+{
+    class Program
+    {
+        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an integer number less than ten billion: ");
+            
+            var input = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(getDigits(input, (input=0)));
+            //  Console.Write($" The number {input} contains number of digits : {1} ", input, getDigits(input, 0));
+
+
+
+
+
+            //  todo - check if Long
+            if (input > 2147483647)
+          {
+              //todo - check if n is less than zero
+              if (input < 0)
+              {
+                  input *= -1;
+              }
+
+          
+
+          if (input > 10000000000)
+              {
+                  Console.WriteLine("Number is greater or equals 10,000,000,000!");
+              }
+          
+
+            //  Console.WriteLine("Number of digits in the number: " + digits);
+
+                }
+           else  {
+                  Console.WriteLine("The number is not a long");
+              }
+
+          
+    }
+        public static int getDigits(int n1, int nodigits)
+        {
+            if (n1 == 0)
+                return nodigits;
+          //  Console.WriteLine("Number of digits in the number: " + digits);
+            return getDigits(n1 / 10, ++nodigits);
+            
+        }
+    }
+}
+    
+
