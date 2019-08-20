9059dd99ddbc8ba35e082e69e58352a5bc039d03
Viesturs Salna <viesturssalna@gmail.com>
2019-08-20 11:06:43 +0300

    added basic excercises

---
+++ basics/Output-10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Output_10
+{
+    class Program
+    {
+        //todo: change the function so it concatinates 3 words together
+        //in Main method display the result.
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Concat());
+
+
+            Console.ReadKey();
+        }
+
+        static string Concat(string w1 = "hello",string w2 = "codelex",string w3 = "world")
+        {
+            return string.Concat(w1+w2+w3);
+        }
+    }
+}
